2ee5cf724920e32b88c9b49996f27ee06bed6b27
karljame <55434286+karljame@users.noreply.github.com>
2020-10-09 11:36:32 +0300

    Create app1.js

---
+++ app1.js
@@ -0,0 +1,39 @@
+let kroon = 1
+const eur = 15.6466
+
+console.log(Math.round(kroon / eur * 100) / 100)
+
+//raadius
+
+let rad = 5
+let area = Math.PI * Math.pow(rad, 2)
+let perimeter = 2 * Math.PI * rad
+console.log(area)
+console.log(perimeter )
+
+
+// 3 ÜL
+
+n = '2'
+let op = parseInt(n) + parseInt(n + n) + parseInt ( n + n + n )
+const answer = (n  + '+' + (n + n) + '+' + (n + n + n ) + '=' +  op)
+console.log(answer)
+
+// 4 ül 
+
+
+
+
+
+// 5ül 
+
+let num1 = 3 
+let num2 = 4
+let num3 = 5
+
+if (num1 > num2)
+    console.log('num1 on surem kui num2')
+else (num2 > num3)
+    console.log('num2 on suurem kui num3')
+else (num3 >num2 )
+    console.log('num 3 on suurem kui n2')
